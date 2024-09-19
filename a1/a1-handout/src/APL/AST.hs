f8d25655f8871c3481605f382109859a90e14c2c
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-09-19 18:47:36 +0300

    Complete task 3

--- a1/a1-handout/src/APL/AST.hs
+++ a1/a1-handout/src/APL/AST.hs
@@ -31,4 +31,23 @@ data Exp
   deriving (Eq, Show)
 
 printExp :: Exp -> String
-printExp = undefined -- TODO
+printExp (CstInt x) = show x
+printExp (CstBool b) = if b then "True" else "False"
+printExp (Var v) = v
+
+printExp (Add e1 e2) = "(" ++ printExp e1 ++ " + " ++ printExp e2 ++ ")"
+printExp (Sub e1 e2) = "(" ++ printExp e1 ++ " - " ++ printExp e2 ++ ")"
+printExp (Mul e1 e2) = "(" ++ printExp e1 ++ " * " ++ printExp e2 ++ ")"
+printExp (Div e1 e2) = "(" ++ printExp e1 ++ " / " ++ printExp e2 ++ ")"
+printExp (Pow e1 e2) = "(" ++ printExp e1 ++ " ^ " ++ printExp e2 ++ ")"
+printExp (Eql e1 e2) = "(" ++ printExp e1 ++ " == " ++ printExp e2 ++ ")"
+
+printExp (If cond e1 e2) = "if " ++ printExp cond ++ " then " ++ printExp e1 ++ " else " ++ printExp e2
+
+printExp (Let v e1 e2) = "let " ++ v ++ " = " ++ printExp e1 ++ " in " ++ printExp e2
+
+printExp (Lambda v body) = "\\" ++ v ++ " -> " ++ printExp body
+
+printExp (Apply e1 e2) = "((" ++ printExp e1 ++ ") (" ++ printExp e2 ++ "))"
+
+printExp (TryCatch e1 e2) = "try " ++ printExp e1 ++  " catch " ++ printExp e2
