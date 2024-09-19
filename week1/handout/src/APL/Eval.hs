c8a428a2675c61aab52e46af15d68a8c197c80ba
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-09-19 17:17:39 +0300

    Propagation of env

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,6 +1,6 @@
 module APL.Eval
   (
-    eval, Val(..), Error
+    eval, Val(..), Error, envEmpty
   )
 where
 
@@ -27,41 +27,41 @@ envExtend var val env = (var, val) : env
 envLookup :: VName -> Env -> Maybe Val
 envLookup var env = lookup var env
 
-eval :: Exp -> Either Error Val
-eval (CstInt x) = Right $ ValInt x
-eval (CstBool x) = Right $ ValBool x
+eval :: Env -> Exp -> Either Error Val
+eval _ (CstInt x) = Right $ ValInt x
+eval _ (CstBool x) = Right $ ValBool x
 
-eval (Eql x y) = do
-  case (eval x, eval y) of
+eval env (Eql x y) = do
+  case (eval env x, eval env y) of
     (Right(ValBool i1), Right(ValBool i2)) -> if i1==i2 then Right (ValBool True) else Right (ValBool False)
     (_, _) -> Left "Operation has failed"
 
-eval (If x y z) = do
-  case eval x of
-    Right(ValBool i1) -> eval y
+eval env (If x y z) = do
+  case eval env x of
+    Right(ValBool i1) -> eval env y
     Right _ -> Left "Operation has failed"
-    _ -> eval z
+    _ -> eval env z
 
-eval (Add x y) = evalHelper (+) x y
-eval (Sub x y) = evalHelper (-) x y
-eval (Mul x y) = evalHelper (*) x y
-eval (Div x y) = evalSafeDiv x y
-eval (Pow x y) = evalSafePow x y
+eval env (Add x y) = evalHelper (+) x y env
+eval env (Sub x y) = evalHelper (-) x y env
+eval env (Mul x y) = evalHelper (*) x y env
+eval env (Div x y) = evalSafeDiv x y env
+eval env (Pow x y) = evalSafePow x y env
 
-evalHelper :: (Integer -> Integer -> Integer) -> Exp -> Exp -> Either Error Val
-evalHelper op x y = do
-  case (eval x, eval y) of
+evalHelper :: (Integer -> Integer -> Integer) -> Exp -> Exp -> Env -> Either Error Val
+evalHelper op x y env = do
+  case (eval env x, eval env y) of
     (Right(ValInt i1), Right(ValInt i2)) -> Right $ ValInt $ op i1 i2
     (_, _) -> Left "Operation has failed"
 
-evalSafeDiv :: Exp -> Exp -> Either Error Val
-evalSafeDiv x y = do
-  case (eval x, eval y) of
+evalSafeDiv :: Exp -> Exp -> Env -> Either Error Val
+evalSafeDiv x y env = do
+  case (eval env x, eval env y) of
     (Right(ValInt i1), Right(ValInt i2)) -> if i2 == 0 then Left "Division by 0" else Right $ ValInt $ div i1 i2
     (_, _) -> Left "Operation has failed"
 
-evalSafePow :: Exp -> Exp -> Either Error Val
-evalSafePow x y = do
-  case (eval x, eval y) of
+evalSafePow :: Exp -> Exp -> Env -> Either Error Val
+evalSafePow x y env = do
+  case (eval env x, eval env y) of
     (Right(ValInt i1), Right(ValInt i2)) -> if i2 < 0 then Left "Negative power" else Right $ ValInt $ (^) i1 i2
     (_, _) -> Left "Operation has failed"
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,7 +1,7 @@
 module APL.Eval_Tests (tests) where
 
 import APL.AST (Exp (..))
-import APL.Eval (Val (..), eval, Error)
+import APL.Eval (Val (..), eval, Error, envEmpty)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 -- import Distribution.Compat.Prelude (Integer)
@@ -31,40 +31,40 @@ intToRight :: Integer -> Either Error Val
 intToRight x = Right $ ValInt x
 
 evalConstantTest :: TestTree
-evalConstantTest = testCase "CstInt to ValInt" $ eval (CstInt 4) @?= intToRight 4
+evalConstantTest = testCase "CstInt to ValInt" $ eval envEmpty (CstInt 4) @?= intToRight 4
 
 evalAddTest :: TestTree
-evalAddTest = testCase "Add 2 constants" $ eval (Add (CstInt 2) (CstInt 3)) @?= intToRight 5
+evalAddTest = testCase "Add 2 constants" $ eval envEmpty (Add (CstInt 2) (CstInt 3)) @?= intToRight 5
 
 evalSubTest :: TestTree
-evalSubTest = testCase "Sub 2 constants" $ eval (Sub (CstInt 2) (CstInt 3)) @?= intToRight (-1)
+evalSubTest = testCase "Sub 2 constants" $ eval envEmpty (Sub (CstInt 2) (CstInt 3)) @?= intToRight (-1)
 
 evalMulTest :: TestTree
-evalMulTest = testCase "Mul 2 constants" $ eval (Mul (CstInt 3) (CstInt 4)) @?= intToRight 12
+evalMulTest = testCase "Mul 2 constants" $ eval envEmpty (Mul (CstInt 3) (CstInt 4)) @?= intToRight 12
 
 evalDivTest :: TestTree
-evalDivTest = testCase "Div 2 constants" $ eval (Div (CstInt 12) (CstInt 3)) @?= intToRight 4
+evalDivTest = testCase "Div 2 constants" $ eval envEmpty (Div (CstInt 12) (CstInt 3)) @?= intToRight 4
 
 evalPowTest :: TestTree
-evalPowTest = testCase "Pow 2 constants" $ eval (Pow (CstInt 2) (CstInt 3)) @?= intToRight 8
+evalPowTest = testCase "Pow 2 constants" $ eval envEmpty (Pow (CstInt 2) (CstInt 3)) @?= intToRight 8
 
 evalExp1Test :: TestTree
-evalExp1Test = testCase "Pow 1 constant and an expression" $ eval (Pow (CstInt 2) (Add (CstInt 3) (CstInt 4))) @?= intToRight 128
+evalExp1Test = testCase "Pow 1 constant and an expression" $ eval envEmpty (Pow (CstInt 2) (Add (CstInt 3) (CstInt 4))) @?= intToRight 128
 
 evalExp2Test :: TestTree
-evalExp2Test = testCase "Add 1 constant and an expression" $ eval (Add (CstInt 2) (Mul (CstInt 3) (CstInt 4))) @?= intToRight 14
+evalExp2Test = testCase "Add 1 constant and an expression" $ eval envEmpty (Add (CstInt 2) (Mul (CstInt 3) (CstInt 4))) @?= intToRight 14
 
 evalExp3Test :: TestTree
-evalExp3Test = testCase "Mul 1 expression and 1 constant" $ eval (Mul (Add (CstInt 2) (CstInt 3)) (CstInt 4)) @?= intToRight 20
+evalExp3Test = testCase "Mul 1 expression and 1 constant" $ eval envEmpty (Mul (Add (CstInt 2) (CstInt 3)) (CstInt 4)) @?= intToRight 20
 
 evalSafeDivTest :: TestTree
-evalSafeDivTest = testCase "Div 2 constants" $ eval (Div (CstInt 12) (CstInt 0)) @?= Left "Division by 0"
+evalSafeDivTest = testCase "Div 2 constants" $ eval envEmpty (Div (CstInt 12) (CstInt 0)) @?= Left "Division by 0"
 
 evalSafePowTest :: TestTree
-evalSafePowTest = testCase "Pow 2 constants" $ eval (Pow (CstInt 2) (CstInt (-1))) @?= Left "Negative power"
+evalSafePowTest = testCase "Pow 2 constants" $ eval envEmpty (Pow (CstInt 2) (CstInt (-1))) @?= Left "Negative power"
 
 evalEqlTest :: TestTree
-evalEqlTest = testCase "Eql int and bool" $ eval (Eql (CstBool True) (CstInt 10)) @?= Left "Operation has failed"
+evalEqlTest = testCase "Eql int and bool" $ eval envEmpty (Eql (CstBool True) (CstInt 10)) @?= Left "Operation has failed"
 
 evalIfTest :: TestTree
-evalIfTest = testCase "If int bool bool" $ eval (If (CstInt 10) (CstBool True) (CstBool False)) @?= Left "Operation has failed"
+evalIfTest = testCase "If int bool bool" $ eval envEmpty (If (CstInt 10) (CstBool True) (CstBool False)) @?= Left "Operation has failed"
