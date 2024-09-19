3e2eeef5d1f1074d9398c7ece532e1eaa7df61d1
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-09-19 23:45:35 +0300

    Complete step 1

--- week2/handout/src/APL/Eval.hs
+++ week2/handout/src/APL/Eval.hs
@@ -28,10 +28,31 @@ envLookup v env = lookup v env
 
 type Error = String
 
-newtype EvalM a = EvalM a -- TODO
+-- a simple data type EvalM and its constructor
+newtype EvalM a = EvalM (Either Error a) 
+
+instance Functor EvalM where
+  fmap f (EvalM(Right x)) = EvalM (Right (f x))
+  fmap _ (EvalM(Left err)) = EvalM(Left err)
+
+instance Applicative EvalM where
+  pure x = EvalM(Right x)
+  -- (<*>) :: EvalM (a -> b) -> EvalM a -> EvalM b 
+  EvalM(Left err) <*> _ = EvalM(Left err)
+  _ <*> EvalM(Left err) = EvalM(Left err)
+  EvalM(Right f) <*> x = fmap f x
+
+instance Monad EvalM where
+  EvalM(Left err) >>= _ = EvalM(Left err)
+  EvalM(Right x) >>= f = f x
 
 runEval :: EvalM a -> Either Error a
-runEval = undefined -- TODO
+runEval x = case x of
+  EvalM(Right x') -> Right x'
+  EvalM(Left err) -> Left err
+
+failure :: String -> EvalM a
+failure s = EvalM(Left s)
 
 eval :: Env -> Exp -> EvalM Val
 eval = undefined -- TODO
