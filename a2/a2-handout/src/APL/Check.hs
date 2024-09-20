ba1df265bed174821bb9e837cf9f9dbb3f07bc82
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-09-20 11:53:32 +0300

    Checking operations that will not throw any errors as they do not contain variables

--- a2/a2-handout/src/APL/Check.hs
+++ a2/a2-handout/src/APL/Check.hs
@@ -21,8 +21,40 @@ instance Monad CheckM where
       Left err -> Left err
       Right x' -> let CheckM y = f x' in y env
 
+askEnv :: CheckM Env
+askEnv = CheckM $ \env -> Right env
+
 check :: Exp -> CheckM ()
-check = undefined
+check (CstInt _) = return ()
+check (CstBool _) = return ()
+check (Add e1 e2) = do
+  check e1
+  check e2
+check (Sub e1 e2) = do
+  check e1
+  check e2
+check (Mul e1 e2) = do
+  check e1
+  check e2
+check (Div e1 e2) = do
+  check e1
+  check e2
+check (Pow e1 e2) = do
+  check e1
+  check e2
+check (Eql e1 e2) = do
+  check e1
+  check e2
+check (If cond e1 e2) = do
+  check cond
+  check e1
+  check e2
+check (Apply e1 e2) = do
+  check e1
+  check e2
+check (TryCatch e1 e2) = do
+  check e1
+  check e2
 
 checkExp :: Exp -> Maybe Error
 checkExp = undefined
