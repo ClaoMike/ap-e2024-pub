b8d18396c36bb73b3999d7f4beacafe3ce2e820f
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-09-20 11:44:19 +0300

    Implement CheckM monad

--- a2/a2-handout/src/APL/Check.hs
+++ a2/a2-handout/src/APL/Check.hs
@@ -1,17 +1,25 @@
 module APL.Check (checkExp, Error) where
 
 import APL.AST (Exp (..), VName)
+import Control.Monad (ap, liftM)
 
 type Error = String
 type Env = [VName]
 
 newtype CheckM a = CheckM (Env -> Either Error a)
 
--- instance Functor CheckM where
+instance Functor CheckM where
+    fmap = liftM
 
--- instance Applicative CheckM where
+instance Applicative CheckM where
+    pure x = CheckM $ \_ -> Right x
+    (<*>) = ap
 
--- instance Monad CheckM where
+instance Monad CheckM where
+  CheckM x >>= f = CheckM $ \env ->
+    case x env of
+      Left err -> Left err
+      Right x' -> let CheckM y = f x' in y env
 
 check :: Exp -> CheckM ()
 check = undefined
